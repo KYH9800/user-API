dfa769a8be5716952a5abbdce585f4203114df8e
고윤혁 <sksgur3217@gmail.com>
2022-12-01 11:13:22 +0900

    done:)

--- prepare/back/routes/users.js
+++ prepare/back/routes/users.js
@@ -6,8 +6,8 @@ const Users = require('../schemas/user');
 // GET /user, 회원 전체 목록 조회
 router.get('/', async (req, res) => {
   try {
-    const result = await Posts.find({}, {});
-    return res.json({ posts: result });
+    const result = await Users.find({}, {});
+    return res.json({ users: result });
   } catch (error) {
     console.error('error: ', error);
     return res.status(400).json({ message: '회원 목록 조회 실패' });
@@ -18,8 +18,8 @@ router.get('/', async (req, res) => {
 router.get('/:userid', async (req, res) => {
   try {
     const { userid } = req.params;
-    const result = await Posts.findOne({ _id: userid }, {});
-    return res.json({ posts: result });
+    const result = await Users.findOne({ _id: userid }, {});
+    return res.json({ users: result });
   } catch (error) {
     console.error('error: ', error);
     return res.status(400).json({ message: '회원 상세 조회 실패' });
